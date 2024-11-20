0e65de50fa7ea4056b13c68eb4b57bd4dea1d455
studev.lop <168014077+juststudevpuc@users.noreply.github.com>
2024-11-20 21:40:34 +0700

    Add files via upload
    vavava

---
+++ js/script.js
@@ -0,0 +1,25 @@
+
+const wrapper = document.querySelector(".wrapper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.innerHTML = "I love you too! 😘";
+  gif.src =
+    "https://stickershop.line-scdn.net/stickershop/v1/product/12608/LINEStorePC/main.png?v=1"
+;
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
